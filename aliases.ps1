f8dabcce8da815ba1fa035e64c37cb2809a97cba
Mased <68379695+MasedMSD@users.noreply.github.com>
2024-09-28 16:51:19 +0500

    Create aliases.ps1

---
+++ aliases.ps1
@@ -0,0 +1,8 @@
+${function:~} = { Set-Location ~ }
+${function:Set-ParentLocation} = { Set-Location .. }; Set-Alias ".." Set-ParentLocation
+${function:...} = { Set-Location ..\.. }
+${function:....} = { Set-Location ..\..\.. }
+${function:.....} = { Set-Location ..\..\..\.. }
+${function:......} = { Set-Location ..\..\..\..\.. }
+
+Set-Alias code code-insiders
